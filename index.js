86441430f974e6115b36592ff8347e9198c9c511
Mubbasher Ashraf <mubashrbhatti@gmail.com>
2020-03-17 16:48:03 +0500

    first commit

--- index.js
+++ index.js
@@ -1,119 +1,69 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var graphql = require('graphql');
 
-var schema = buildSchema(`
-    input MessageInput {
-        content: String
-        author: String
-    },
-    type Message {
-        id: ID!
-        content: String
-        author: String
-    },
-    type Randomdie {
-        numSides: Int!
-        rollOnce: Int!
-        roll(numRolls: Int!): [Int]
-    },
-    type Query {
-        ip: String
-        quoteOfTheDay: String
-        random: Float!
-        getDie(numSides: Int): Randomdie
-        getMessage(id: ID!): Message
-    },
+// var schema = buildSchema(`
+//     type User {
+//         id: String
+//         name: String
+//     },
 
-    type Mutation {
-        createMessage(input: MessageInput): Message
-        updateMessage(id: ID!, input: MessageInput): Message
-    }
-`);
+//     type Query {
+//         user(id: String): User
+//     }
+// `);
 
-class Message {
-    constructor(id, { content, author }) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-}
 
-class RandomDie {
-    constructor(numSides) {
-        this.numSides = numSides;
-    }
-    rollOnce() {
-        return 1 + Math.floor(Math.random() * this.numSides);
-    }
-    roll({ numRolls }) {
-        var output = [];
-        for (var i = 0; i < numRolls; i++) {
-            output.push(this.rollOnce());
-        }
-        return output;
-    }
-}
-
-var fakeDatabase = {};
-
-var root = {
-    ip: function(args, request) {
-        return request.ip;
-    },
-    quoteOfTheDay: () => {
-        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within!';
-    },
-    random: () => {
-        return Math.random();
-    },
-    setMessage: ({ message }) => {
-        fakeDatabase.message = message;
-        return message;
-    },
-    getMessage: ({ id }) => {
-        if(!fakeDatabase[id]) {
-            throw new Error('No message found with id : ' + id);
-        }
-        return new Message(id, fakeDatabase[id]);
-    },
-    createMessage: ({ input }) => {
-        var id = require('crypto').randomBytes(10).toString('hex')
-
-        fakeDatabase[id] = input;
-        console.log(fakeDatabase)
-        return new Message(id, input);
-    },
-    updateMessage: ({ id, input }) => {
-        if(!fakeDatabase[id]) {
-            throw new Error('No message found with id : ' + id);
-        }
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    },
-    rollDice: ({ numDice, numSides }) => {
-        var output = [];
-        for (var i = 0; i < numDice ; i++) {
-            output.push(1 + Math.floor(Math.random() * (numSides || 6) ));   
-        }
-        return output;
-    },
-    getDie: ({ numSides }) => {
-        return new RandomDie(numSides || 6);
-    }
+var fakeDatabase = {
+    'a': {
+        id: 'a',
+        name: 'alice',
+      },
+      'b': {
+        id: 'b',
+        name: 'bob',
+      },
 };
 
-const loggingMiddleware = (req, res, next) => {
-    console.log('ip:', req.ip);
-    next();
-}
+const userType = new graphql.GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: graphql.GraphQLString },
+        name: { type: graphql.GraphQLString },
+    }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        user: {
+            type: userType,
+            args: {
+                id: { type: graphql.GraphQLString }
+            },
+            resolve: (_, { id }) => {
+                return fakeDatabase[id];
+            }
+        }
+    }
+});
+
+// const loggingMiddleware = (req, res, next) => {
+//     console.log('ip:', req.ip);
+//     next();
+// }
 
 var app = express();
-app.use(loggingMiddleware);
+// app.use(loggingMiddleware);
+var schema = new graphql.GraphQLSchema({ query: queryType });
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    // rootValue: root,
+    //context: any
+    //pretty: boolean
+    //formatError: ? Function
+    //validationRules: ? Array
     graphiql: true,
 }));
 
